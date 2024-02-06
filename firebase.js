cdc7b7b4ee6a629d0121f1b7f3cfc818d16df4d6
wirke <90754297+wirke@users.noreply.github.com>
2024-02-06 02:34:37 +0100

    workflow deployment

--- firebase.js
+++ firebase.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getPerformance } from "firebase/performance";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBbxIHC6MaeCRyyu0WW68GucyzLr_qfaLo",
   authDomain: "bioskop-46a84.firebaseapp.com",
@@ -16,6 +12,6 @@ const firebaseConfig = {
   measurementId: "G-MWT0HSTNDJ"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const perf = getPerformance(app);
