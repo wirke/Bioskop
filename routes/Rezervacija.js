80e36e31c61c4457c6719fc2d0864c5287dd36be
wirke <90754297+wirke@users.noreply.github.com>
2024-04-14 17:23:08 +0200

    MVC za Rezervacija komponentu

--- routes/Rezervacija.js
+++ routes/Rezervacija.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bookingController = require('../controllers/bookingController');
+const bookingController = require('../controllers/Rezervacija');
 
 router.post('/bookings', bookingController.createBooking);
 router.get('/bookings/:id', bookingController.getBookingById);
