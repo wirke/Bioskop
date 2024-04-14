0e47067dac2fa975525899c27370a8802e042a3d
wirke <90754297+wirke@users.noreply.github.com>
2024-04-14 17:22:43 +0200

    MVC za Korisnik komponentu

--- controllers/Korisnik.js
+++ controllers/Korisnik.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const User = require('../models/Korisnik');
 
 const userController = {
     createUser: async (req, res) => {
--- routes/Korisnik.js
+++ routes/Korisnik.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/userController');
+const userController = require('../controllers/Korisnik');
 const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/users', userController.createUser);
