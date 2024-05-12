ff9b0cfb0c8263404b618795fa2cd697555983a8
kristinastev <149689425+kristinastev@users.noreply.github.com>
2024-05-12 20:48:08 +0200

    FilmKartica kreirana

---
+++ frontend/src/app/film-kartica/film-kartica.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-film-kartica',
+  standalone: true,
+  imports: [],
+  templateUrl: './film-kartica.component.html',
+  styleUrl: './film-kartica.component.css'
+})
+export class FilmKarticaComponent {
+
+}
