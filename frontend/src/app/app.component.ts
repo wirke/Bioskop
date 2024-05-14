a95d5c171786510eb64e2841e13d21ddb65e992b
kristinastev <149689425+kristinastev@users.noreply.github.com>
2024-05-14 16:46:20 +0200

    izbrisani fajlovi

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FilmKarticaComponent } from './film-kartica/film-kartica.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FilmKarticaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
