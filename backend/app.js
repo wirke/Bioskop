d93660159dfea062629647bf50653a0ab270fdef
wirke <90754297+wirke@users.noreply.github.com>
2024-05-21 10:03:21 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const movieRouter = require('./routes/movies');
 const userRouter = require('./routes/users');
 const bookingRouter = require('./routes/bookings');
 
+dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-const uri = "mongodb+srv://wiriyevich:cavuh9UCvo10rbvI@cluster0.nzgsmre.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = 'mongodb+srv://wiriyevich:cavuh9UCvo10rbvI@cluster0.nzgsmre.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 mongoose.connect(uri, {
   dbName: 'Bioskop',
@@ -23,7 +25,7 @@ mongoose.connect(uri, {
 app.use(express.json());
 app.use('/movies', movieRouter);
 app.use('/users', userRouter);
-app.use('booking', bookingRouter);
+app.use('/bookings', bookingRouter);
 
 app.use(function(err, req, res, next) {
   console.error(err.stack);
