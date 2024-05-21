4fffe4c90336652a0ae7dee54fd39748e94bf3fd
wirke <90754297+wirke@users.noreply.github.com>
2024-05-21 11:18:26 +0200

    rezervacija sedista

--- backend/routes/bookings.js
+++ backend/routes/bookings.js
@@ -23,11 +23,33 @@ router.post('/', async (req, res) => {
     const { id_user, id_movie, id_screening, reserved_seats } = req.body;
 
 // Ruta za pregled svih rezervacija od korisnika
+    try {
+        const existingBookings = await Booking.find({ id_screening });
+
+        // Provera zauzetih sedišta
+        const occupiedSeats = existingBookings.flatMap(booking => booking.reserved_seats.map(seat => seat.seat));
+        const alreadyOccupied = reserved_seats.some(seat => occupiedSeats.includes(seat.seat));
 
 // Ruta za pregled rezervacija za korisnika za film
+        if (alreadyOccupied) {
+            return res.status(400).json({ message: "Some seats are already occupied" });
+        }
 
 // Ruta za rezervaciju emitovanja za film od korisnika
+        const booking = new Booking({
+            id_user,
+            id_movie,
+            id_screening,
+            reserved_seats
+        });
 
 // Ruta za pregled rezervacije po ID-u
+        await booking.save();
+        res.status(201).json(booking);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
 
 module.exports   = router;
