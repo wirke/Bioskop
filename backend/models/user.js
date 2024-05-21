8e025978e21c484e81e2e12c64bbab213a1e9fec
wirke <90754297+wirke@users.noreply.github.com>
2024-05-21 11:15:54 +0200

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -30,3 +30,4 @@ userSchema.methods.matchPassword = async function(enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
 
+module.exports = mongoose.model('User', userSchema);
