de7992e45367c028046516a1f1fd17d1447520a6
wirke <90754297+wirke@users.noreply.github.com>
2024-05-21 11:15:42 +0200

    Bcrypt implementacija u modelu

--- backend/models/user.js
+++ backend/models/user.js
@@ -17,14 +17,6 @@ const userSchema = new mongoose.Schema({
     },
 }, { collection: 'Korisnik' });
 
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    } catch (err) {
-        next(err);
 userSchema.pre('save', async function(next) {
     if (!this.isModified('password')) {
         return next();
@@ -34,10 +26,7 @@ userSchema.pre('save', async function(next) {
     next();
 });
 
-userSchema.methods.comparePassword = function (candidatePassword) {
-    return bcrypt.compare(candidatePassword, this.password);
 userSchema.methods.matchPassword = async function(enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
 
-module.exports = mongoose.model('User', userSchema);module.exports = mongoose.model('User', userSchema);
