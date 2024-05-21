9bfeaebc7487a01eb765d73834b9155ab74d3797
wirke <90754297+wirke@users.noreply.github.com>
2024-05-21 11:08:26 +0200

    uklanjanje modela za id

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,11 +2,6 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-    _id: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        auto: true,
-    },
     username: {
         type: String,
         required: true,
@@ -32,11 +27,19 @@ userSchema.pre('save', async function (next) {
         next();
     } catch (err) {
         next(err);
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+        return next();
     }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
 });
 
 userSchema.methods.comparePassword = function (candidatePassword) {
     return bcrypt.compare(candidatePassword, this.password);
+userSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
 };
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema);module.exports = mongoose.model('User', userSchema);
