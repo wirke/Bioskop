923e2e77981b7e96eaf9b93dd173cbcd7e6360ed
kristinastev <149689425+kristinastev@users.noreply.github.com>
2024-06-02 10:55:06 +0200

    route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { FilmKarticaComponent } from './film-kartica/film-kartica.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'Film',
+        component: FilmKarticaComponent,
+        title : 'Pls radi'
+    }
+];
